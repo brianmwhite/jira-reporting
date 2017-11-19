924964c3acb05320d8a7deb4626aaedc56cf80c4
Brian White <brian@thirdember.com>
2017-11-19 15:42:10 -0500

    add end-to-end test with tab output

---
+++ test/jr.common.tests/TimeSummarizationTests.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using Xunit;
+
+namespace jr.common.tests
+{
+    public class TimeSummarizationTests
+    {
+            // TODO: split apart this test into multiple once code is refactored
+            [Fact]
+            public void ExcelToTextOutputTest()
+            {
+                ExcelInput excel = new ExcelInput();
+                List<WorkItem> workItems = excel.ReadExcel(TestUtils.GetStreamFromResource("sample.xls"));
+                TimeSummarization ts = new TimeSummarization(
+                workItems
+                , _devRate: 0
+                , _mgmtRate: 0
+                , _mgmtUsers: null
+                , _splitPO: false
+                , _projectTextToTrim: ""
+            );
+
+            ts.SummarizeWorkItems();
+            string output = ts.GenerateSeparatedValueTextOutput('\t', "Project,Dev_Hours,Dev_Amount,Mgmt_Hours,Mgmt_Amount,Total_Hours,Total_Amount");
+            string expectedOutput = TestUtils.GetTextFromResource("sample-tab-output.txt");
+            Assert.Equal(expectedOutput, output);
+            }
+    }
+}
