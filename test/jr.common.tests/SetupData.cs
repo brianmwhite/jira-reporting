b99be02ffe9ae7882c504ab321e8257922b83c9f
Brian White <brian@thirdember.com>
2017-12-05 20:21:32 -0500

    setup test for issue grouping

--- test/jr.common.tests/SetupData.cs
+++ test/jr.common.tests/SetupData.cs
@@ -2,9 +2,8 @@ using System.Collections.Generic;
 
 namespace jr.common.tests
 {
-    public class SetupData
+    public static class SetupData
     {
-
         public static List<string[]>  CreateStringArrayListWithProjectGrouping() {
             List<string[]> s = new List<string[]>();
             s.Add(new string[] {"Project","Dev Hours","Dev Amount","Mgmt Hours","Mgmt Amount","Total Hours","Total Amount"});
@@ -12,22 +11,72 @@ namespace jr.common.tests
             s.Add(new string[] {"Alpha Beta - Project 2 #ABC00006","12.00","$0.00","0.00","$0.00","12.00","$0.00"});
             s.Add(new string[] {"Alpha Beta - Project 3 #ABC00008","25.00","$0.00","1.50","$0.00","26.50","$0.00"});
             s.Add(new string[] {"Alpha Beta - Project 4 #ABC00004","0.50","$0.00","0.00","$0.00","0.50","$0.00"});
-            s.Add(new string[] {"Total","92.00","$0.00","5.50","$0.00","97.50","$0.00"});
+            s.Add(new string[] {"","92.00","$0.00","5.50","$0.00","97.50","$0.00"});
+            return s;
+        }
+        
+        public static List<string[]>  CreateStringArrayListWithIssueGrouping() {
+            List<string[]> s = new List<string[]>();
+            s.Add(new string[] {"Project","Issue","Dev Hours","Dev Amount","Mgmt Hours","Mgmt Amount","Total Hours","Total Amount"});
+            s.Add(new string[] {"Alpha Beta - Project 1 #ABC00001","[ABC-345] orci. Donec nibh.","5.00","$0.00","4.00","$0.00","9.00","$0.00"});
+            s.Add(new string[] {"Alpha Beta - Project 1 #ABC00001","[ABC-415] fames ac turpis","2.50","$0.00","0.00","$0.00","2.50","$0.00"});
+            s.Add(new string[] {"Alpha Beta - Project 1 #ABC00001","[ABC-419] Nullam ut nisi a odio semper cursus.","1.00","$0.00","0.00","$0.00","1.00","$0.00"});
+            s.Add(new string[] {"Alpha Beta - Project 1 #ABC00001","[ABC-423] lacus. Aliquam rutrum lorem","1.00","$0.00","0.00","$0.00","1.00","$0.00"});
+            s.Add(new string[] {"Alpha Beta - Project 1 #ABC00001","[ABC-443] enim nisl","3.50","$0.00","0.00","$0.00","3.50","$0.00"});
+            s.Add(new string[] {"Alpha Beta - Project 1 #ABC00001","[ABC-444] hendrerit id, ante. Nunc mauris sapien, cursus in, hendrerit","12.50","$0.00","0.00","$0.00","12.50","$0.00"});
+            s.Add(new string[] {"Alpha Beta - Project 1 #ABC00001","[ABC-445] dolor. Quisque tincidunt pede ac urna. Ut","0.50","$0.00","0.00","$0.00","0.50","$0.00"});
+            s.Add(new string[] {"Alpha Beta - Project 1 #ABC00001","[ABC-446] semper cursus. Integer mollis. Integer tincidunt aliquam arcu. Aliquam","9.00","$0.00","0.00","$0.00","9.00","$0.00"});
+            s.Add(new string[] {"Alpha Beta - Project 1 #ABC00001","[ABC-448] Aliquam adipiscing lobortis risus. In mi pede, nonummy","1.00","$0.00","0.00","$0.00","1.00","$0.00"});
+            s.Add(new string[] {"Alpha Beta - Project 1 #ABC00001","[ABC-456] at lacus. Quisque purus sapien,","7.00","$0.00","0.00","$0.00","7.00","$0.00"});
+            s.Add(new string[] {"Alpha Beta - Project 1 #ABC00001","[ABC-459] vitae velit egestas lacinia.","1.00","$0.00","0.00","$0.00","1.00","$0.00"});
+            s.Add(new string[] {"Alpha Beta - Project 1 #ABC00001","[ABC-460] tempor lorem, eget mollis lectus","1.50","$0.00","0.00","$0.00","1.50","$0.00"});
+            s.Add(new string[] {"Alpha Beta - Project 1 #ABC00001","[ABC-461] Donec tempus, lorem fringilla","4.00","$0.00","0.00","$0.00","4.00","$0.00"});
+            s.Add(new string[] {"Alpha Beta - Project 1 #ABC00001","[ABC-462] placerat eget,","1.00","$0.00","0.00","$0.00","1.00","$0.00"});
+            s.Add(new string[] {"Alpha Beta - Project 1 #ABC00001","[ABC-463] non, luctus sit amet, faucibus","4.00","$0.00","0.00","$0.00","4.00","$0.00"});
+            s.Add(new string[] {"Alpha Beta - Project 2 #ABC00006","[BCD-106] turpis egestas. Aliquam fringilla","4.50","$0.00","0.00","$0.00","4.50","$0.00"});
+            s.Add(new string[] {"Alpha Beta - Project 2 #ABC00006","[BCD-16] ut, molestie in, tempus","0.50","$0.00","0.00","$0.00","0.50","$0.00"});
+            s.Add(new string[] {"Alpha Beta - Project 2 #ABC00006","[BCD-250] pellentesque a, facilisis non, bibendum","5.50","$0.00","0.00","$0.00","5.50","$0.00"});
+            s.Add(new string[] {"Alpha Beta - Project 2 #ABC00006","[BCD-253] cursus, diam at pretium aliquet, metus urna","1.50","$0.00","0.00","$0.00","1.50","$0.00"});
+            s.Add(new string[] {"Alpha Beta - Project 3 #ABC00008","[CDE-263] dictum augue malesuada malesuada. Integer id magna","5.00","$0.00","1.50","$0.00","6.50","$0.00"});
+            s.Add(new string[] {"Alpha Beta - Project 3 #ABC00008","[CDE-844] risus. Donec egestas. Aliquam nec","0.50","$0.00","0.00","$0.00","0.50","$0.00"});
+            s.Add(new string[] {"Alpha Beta - Project 3 #ABC00008","[CDE-872] sagittis. Nullam vitae diam. Proin","0.50","$0.00","0.00","$0.00","0.50","$0.00"});
+            s.Add(new string[] {"Alpha Beta - Project 3 #ABC00008","[CDE-886] dignissim. Maecenas ornare egestas","1.00","$0.00","0.00","$0.00","1.00","$0.00"});
+            s.Add(new string[] {"Alpha Beta - Project 3 #ABC00008","[CDE-897] ac metus vitae velit egestas lacinia. Sed congue,","0.50","$0.00","0.00","$0.00","0.50","$0.00"});
+            s.Add(new string[] {"Alpha Beta - Project 3 #ABC00008","[CDE-903] sociis natoque","4.50","$0.00","0.00","$0.00","4.50","$0.00"});
+            s.Add(new string[] {"Alpha Beta - Project 3 #ABC00008","[CDE-904] erat. Vivamus nisi. Mauris nulla. Integer","2.50","$0.00","0.00","$0.00","2.50","$0.00"});
+            s.Add(new string[] {"Alpha Beta - Project 3 #ABC00008","[CDE-905] tincidunt. Donec vitae erat vel","4.50","$0.00","0.00","$0.00","4.50","$0.00"});
+            s.Add(new string[] {"Alpha Beta - Project 3 #ABC00008","[CDE-906] Nunc ullamcorper,","6.00","$0.00","0.00","$0.00","6.00","$0.00"});
+            s.Add(new string[] {"Alpha Beta - Project 4 #ABC00004","[EFG-26] faucibus ut,","0.50","$0.00","0.00","$0.00","0.50","$0.00"});
+            s.Add(new string[] {"","","92.00","$0.00","5.50","$0.00","97.50","$0.00"});
             return s;
         }
 
-        public static List<SummarizedItem> CreateSummarizedItemList()
+        public static List<SummarizedItem> CreateSummarizedItemListIssueGroupingWithTotals()
         {
             List<SummarizedItem> si = new List<SummarizedItem>();
-            AddSummarizedProjectDataRows(si);
+            AddSummarizedIssueDataRows(si);
+            AddTotalSummarizedRow(si);
             return si;
         }
-        
-        public static List<SummarizedItem> CreateSummarizedItemListWithTotals()
+
+        private static void AddTotalSummarizedRow(List<SummarizedItem> si)
+        {
+            si.Add(new SummarizedItem()
+            {
+                project = "",
+                issue = "",
+                dev_rate = 0,
+                mgmt_rate = 0,
+                dev_hours = 92,
+                mgmt_hours = 5.5
+            });
+        }
+
+        public static List<SummarizedItem> CreateSummarizedItemListProjectGroupingWithTotals()
         {
             List<SummarizedItem> si = new List<SummarizedItem>();
             AddSummarizedProjectDataRows(si);
-            si.Add(new SummarizedItem() { project = "Total", dev_rate = 0, mgmt_rate = 0, dev_hours = 92, mgmt_hours = 5.5 });
+            AddTotalSummarizedRow(si);
             return si;
         }
 
@@ -37,6 +86,42 @@ namespace jr.common.tests
             si.Add(new SummarizedItem() { project = "Alpha Beta - Project 2 #ABC00006", dev_rate = 0, mgmt_rate = 0, dev_hours = 12, mgmt_hours = 0.00 });
             si.Add(new SummarizedItem() { project = "Alpha Beta - Project 3 #ABC00008", dev_rate = 0, mgmt_rate = 0, dev_hours = 25, mgmt_hours = 1.50 });
             si.Add(new SummarizedItem() { project = "Alpha Beta - Project 4 #ABC00004", dev_rate = 0, mgmt_rate = 0, dev_hours = 0.5, mgmt_hours = 0.00 });
+        }
+
+        private static void AddSummarizedIssueDataRows(List<SummarizedItem> si)
+        {
+            //sample output example
+            //output.AppendLine($"si.Add(new SummarizedItem() {{ project = \"{item.project}\", issue = \"{item.issue}\", dev_hours = {item.dev_hours}, mgmt_hours = {item.mgmt_hours}, dev_rate = 0, mgmt_rate = 0}});");
+            
+            si.Add(new SummarizedItem() { project = "Alpha Beta - Project 1 #ABC00001", issue = "[ABC-345] orci. Donec nibh.", dev_hours = 5, mgmt_hours = 4, dev_rate = 0, mgmt_rate = 0});
+            si.Add(new SummarizedItem() { project = "Alpha Beta - Project 1 #ABC00001", issue = "[ABC-415] fames ac turpis", dev_hours = 2.5, mgmt_hours = 0, dev_rate = 0, mgmt_rate = 0});
+            si.Add(new SummarizedItem() { project = "Alpha Beta - Project 1 #ABC00001", issue = "[ABC-419] Nullam ut nisi a odio semper cursus.", dev_hours = 1, mgmt_hours = 0, dev_rate = 0, mgmt_rate = 0});
+            si.Add(new SummarizedItem() { project = "Alpha Beta - Project 1 #ABC00001", issue = "[ABC-423] lacus. Aliquam rutrum lorem", dev_hours = 1, mgmt_hours = 0, dev_rate = 0, mgmt_rate = 0});
+            si.Add(new SummarizedItem() { project = "Alpha Beta - Project 1 #ABC00001", issue = "[ABC-443] enim nisl", dev_hours = 3.5, mgmt_hours = 0, dev_rate = 0, mgmt_rate = 0});
+            si.Add(new SummarizedItem() { project = "Alpha Beta - Project 1 #ABC00001", issue = "[ABC-444] hendrerit id, ante. Nunc mauris sapien, cursus in, hendrerit", dev_hours = 12.5, mgmt_hours = 0, dev_rate = 0, mgmt_rate = 0});
+            si.Add(new SummarizedItem() { project = "Alpha Beta - Project 1 #ABC00001", issue = "[ABC-445] dolor. Quisque tincidunt pede ac urna. Ut", dev_hours = 0.5, mgmt_hours = 0, dev_rate = 0, mgmt_rate = 0});
+            si.Add(new SummarizedItem() { project = "Alpha Beta - Project 1 #ABC00001", issue = "[ABC-446] semper cursus. Integer mollis. Integer tincidunt aliquam arcu. Aliquam", dev_hours = 9, mgmt_hours = 0, dev_rate = 0, mgmt_rate = 0});
+            si.Add(new SummarizedItem() { project = "Alpha Beta - Project 1 #ABC00001", issue = "[ABC-448] Aliquam adipiscing lobortis risus. In mi pede, nonummy", dev_hours = 1, mgmt_hours = 0, dev_rate = 0, mgmt_rate = 0});
+            si.Add(new SummarizedItem() { project = "Alpha Beta - Project 1 #ABC00001", issue = "[ABC-456] at lacus. Quisque purus sapien,", dev_hours = 7, mgmt_hours = 0, dev_rate = 0, mgmt_rate = 0});
+            si.Add(new SummarizedItem() { project = "Alpha Beta - Project 1 #ABC00001", issue = "[ABC-459] vitae velit egestas lacinia.", dev_hours = 1, mgmt_hours = 0, dev_rate = 0, mgmt_rate = 0});
+            si.Add(new SummarizedItem() { project = "Alpha Beta - Project 1 #ABC00001", issue = "[ABC-460] tempor lorem, eget mollis lectus", dev_hours = 1.5, mgmt_hours = 0, dev_rate = 0, mgmt_rate = 0});
+            si.Add(new SummarizedItem() { project = "Alpha Beta - Project 1 #ABC00001", issue = "[ABC-461] Donec tempus, lorem fringilla", dev_hours = 4, mgmt_hours = 0, dev_rate = 0, mgmt_rate = 0});
+            si.Add(new SummarizedItem() { project = "Alpha Beta - Project 1 #ABC00001", issue = "[ABC-462] placerat eget,", dev_hours = 1, mgmt_hours = 0, dev_rate = 0, mgmt_rate = 0});
+            si.Add(new SummarizedItem() { project = "Alpha Beta - Project 1 #ABC00001", issue = "[ABC-463] non, luctus sit amet, faucibus", dev_hours = 4, mgmt_hours = 0, dev_rate = 0, mgmt_rate = 0});
+            si.Add(new SummarizedItem() { project = "Alpha Beta - Project 2 #ABC00006", issue = "[BCD-106] turpis egestas. Aliquam fringilla", dev_hours = 4.5, mgmt_hours = 0, dev_rate = 0, mgmt_rate = 0});
+            si.Add(new SummarizedItem() { project = "Alpha Beta - Project 2 #ABC00006", issue = "[BCD-16] ut, molestie in, tempus", dev_hours = 0.5, mgmt_hours = 0, dev_rate = 0, mgmt_rate = 0});
+            si.Add(new SummarizedItem() { project = "Alpha Beta - Project 2 #ABC00006", issue = "[BCD-250] pellentesque a, facilisis non, bibendum", dev_hours = 5.5, mgmt_hours = 0, dev_rate = 0, mgmt_rate = 0});
+            si.Add(new SummarizedItem() { project = "Alpha Beta - Project 2 #ABC00006", issue = "[BCD-253] cursus, diam at pretium aliquet, metus urna", dev_hours = 1.5, mgmt_hours = 0, dev_rate = 0, mgmt_rate = 0});
+            si.Add(new SummarizedItem() { project = "Alpha Beta - Project 3 #ABC00008", issue = "[CDE-263] dictum augue malesuada malesuada. Integer id magna", dev_hours = 5, mgmt_hours = 1.5, dev_rate = 0, mgmt_rate = 0});
+            si.Add(new SummarizedItem() { project = "Alpha Beta - Project 3 #ABC00008", issue = "[CDE-844] risus. Donec egestas. Aliquam nec", dev_hours = 0.5, mgmt_hours = 0, dev_rate = 0, mgmt_rate = 0});
+            si.Add(new SummarizedItem() { project = "Alpha Beta - Project 3 #ABC00008", issue = "[CDE-872] sagittis. Nullam vitae diam. Proin", dev_hours = 0.5, mgmt_hours = 0, dev_rate = 0, mgmt_rate = 0});
+            si.Add(new SummarizedItem() { project = "Alpha Beta - Project 3 #ABC00008", issue = "[CDE-886] dignissim. Maecenas ornare egestas", dev_hours = 1, mgmt_hours = 0, dev_rate = 0, mgmt_rate = 0});
+            si.Add(new SummarizedItem() { project = "Alpha Beta - Project 3 #ABC00008", issue = "[CDE-897] ac metus vitae velit egestas lacinia. Sed congue,", dev_hours = 0.5, mgmt_hours = 0, dev_rate = 0, mgmt_rate = 0});
+            si.Add(new SummarizedItem() { project = "Alpha Beta - Project 3 #ABC00008", issue = "[CDE-903] sociis natoque", dev_hours = 4.5, mgmt_hours = 0, dev_rate = 0, mgmt_rate = 0});
+            si.Add(new SummarizedItem() { project = "Alpha Beta - Project 3 #ABC00008", issue = "[CDE-904] erat. Vivamus nisi. Mauris nulla. Integer", dev_hours = 2.5, mgmt_hours = 0, dev_rate = 0, mgmt_rate = 0});
+            si.Add(new SummarizedItem() { project = "Alpha Beta - Project 3 #ABC00008", issue = "[CDE-905] tincidunt. Donec vitae erat vel", dev_hours = 4.5, mgmt_hours = 0, dev_rate = 0, mgmt_rate = 0});
+            si.Add(new SummarizedItem() { project = "Alpha Beta - Project 3 #ABC00008", issue = "[CDE-906] Nunc ullamcorper,", dev_hours = 6, mgmt_hours = 0, dev_rate = 0, mgmt_rate = 0});
+            si.Add(new SummarizedItem() { project = "Alpha Beta - Project 4 #ABC00004", issue = "[EFG-26] faucibus ut,", dev_hours = 0.5, mgmt_hours = 0, dev_rate = 0, mgmt_rate = 0});
         }
 
         public static List<WorkItem> CreateWorkItemList()
--- test/jr.common.tests/TimeSummarizationTests.cs
+++ test/jr.common.tests/TimeSummarizationTests.cs
@@ -1,5 +1,7 @@
+using System;
 using Xunit;
 using System.Linq;
+using System.Text;
 
 namespace jr.common.tests
 {
@@ -44,6 +46,7 @@ namespace jr.common.tests
             );
             
             var si = timeSummarizationData.SummarizeWorkItems(wi);
+            
             Assert.Equal(29, si.Count);
             Assert.Equal(92, si.Sum(x => x.dev_hours));
             Assert.Equal(5.5, si.Sum(x => x.mgmt_hours));
@@ -53,7 +56,7 @@ namespace jr.common.tests
         [Fact]
         public void SummarizedItemsToStringArrayTest()
         {
-            var si = SetupData.CreateSummarizedItemListWithTotals();
+            var si = SetupData.CreateSummarizedItemListProjectGroupingWithTotals();
             
             string[] mgmt = { "bwall" };
             var timeSummarizationData = new TimeSummarization(
@@ -71,12 +74,44 @@ namespace jr.common.tests
             Assert.Equal(expectedOutput, strOutput);
         }
 
+        [Fact(Skip = "Not ready")]
+        public void IssueTest()
+        {
+            var wi = SetupData.CreateWorkItemList();
+            
+            string[] mgmt = { "bwall" };
+            var timeSummarizationData = new TimeSummarization(
+                devRate: 0
+                , mgmtRate: 0
+                , mgmtUsers: mgmt
+                , splitPo: false
+                , projectTextToTrim: ""
+                , outputColumns: "Project,Issue,Dev_Hours,Dev_Amount,Mgmt_Hours,Mgmt_Amount,Total_Hours,Total_Amount"
+                , groupByString: "issue"
+            );
+            
+            var si = timeSummarizationData.SummarizeWorkItems(wi);
+
+            var od = timeSummarizationData.GenerateOutputData(si);
+            var st = TimeSummarization.GenerateSeparatedValueTextOutput(od, '\t');
+            
+//            Console.WriteLine(st);
+//            var output = new StringBuilder();
+//            foreach (var item in si)
+//            {
+//                output.AppendLine($"si.Add(new SummarizedItem() {{ project = \"){item.project}\", issue = \"{item.issue}\", dev_hours = {item.dev_hours}, mgmt_hours = {item.mgmt_hours}, dev_rate = 0, mgmt_rate = 0}});");
+//            }
+//            string o = output.ToString();
+//            Console.WriteLine(o);
+        }
+        
+
         [Fact]
         public void TabSeparatedOutputTest()
         {
             var strOutput = SetupData.CreateStringArrayListWithProjectGrouping();
             var output = TimeSummarization.GenerateSeparatedValueTextOutput(strOutput, '\t');
-            var expectedOutput = TestUtils.GetTextFromResource("sample-tab-output.txt");
+            var expectedOutput = TestUtils.GetTextFromResource("sample-tab-output-project.txt");
 
             Assert.Equal(expectedOutput, output);
         }
