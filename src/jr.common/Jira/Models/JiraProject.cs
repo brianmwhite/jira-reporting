be6066043b73e71b6aa5bbf115cadc8e42a777ec
Brian White <brian@thirdember.com>
2017-12-14 15:15:39 -0500

    remove unused properties

--- src/jr.common/Jira/Models/JiraProject.cs
+++ src/jr.common/Jira/Models/JiraProject.cs
@@ -4,9 +4,6 @@ namespace jr.common.Jira.Models
 {
     public class JiraProject
     {
-//        [JsonProperty("components")]
-//        public Component[] Components { get; set; }
-
         [JsonProperty("description")]
         public string Description { get; set; }
 
@@ -18,47 +15,5 @@ namespace jr.common.Jira.Models
 
         [JsonProperty("name")]
         public string Name { get; set; }
-
-//        [JsonProperty("versions")]
-//        public Version[] Versions { get; set; }
     }
-
-//    public class Version
-//    {
-//        [JsonProperty("archived")]
-//        public bool Archived { get; set; }
-//
-//        [JsonProperty("description")]
-//        public string Description { get; set; }
-//
-//        [JsonProperty("id")]
-//        public string Id { get; set; }
-//
-//        [JsonProperty("name")]
-//        public string Name { get; set; }
-//
-//        [JsonProperty("projectId")]
-//        public long ProjectId { get; set; }
-//
-//        [JsonProperty("releaseDate")]
-//        public string ReleaseDate { get; set; }
-//
-//        [JsonProperty("released")]
-//        public bool Released { get; set; }
-//
-//        [JsonProperty("userReleaseDate")]
-//        public string UserReleaseDate { get; set; }
-//    }
-//
-//    public class Component
-//    {
-//        [JsonProperty("description")]
-//        public string Description { get; set; }
-//
-//        [JsonProperty("id")]
-//        public string Id { get; set; }
-//
-//        [JsonProperty("name")]
-//        public string Name { get; set; }
-//    }
 }
--- src/jr.common/Jira/Models/TempoWorkItem.cs
+++ src/jr.common/Jira/Models/TempoWorkItem.cs
@@ -10,29 +10,11 @@ namespace jr.common.Jira.Models
         [JsonProperty("billedSeconds")]
         public long BilledSeconds { get; set; }
 
-//        [JsonProperty("comment")]
-//        public string Comment { get; set; }
-
-//        [JsonProperty("dateCreated")]
-//        public string DateCreated { get; set; }
-
-//        [JsonProperty("dateStarted")]
-//        public string DateStarted { get; set; }
-
-//        [JsonProperty("dateUpdated")]
-//        public string DateUpdated { get; set; }
-
         [JsonProperty("id")]
         public long Id { get; set; }
 
         [JsonProperty("issue")]
         public Issue Issue { get; set; }
-
-//        [JsonProperty("jiraWorklogId")]
-//        public long? JiraWorklogId { get; set; }
-
-//        [JsonProperty("tempoWorklogId")]
-//        public long TempoWorklogId { get; set; }
 
         [JsonProperty("timeSpentSeconds")]
         public long TimeSpentSeconds { get; set; }
@@ -52,12 +34,6 @@ namespace jr.common.Jira.Models
         [JsonProperty("projectId")]
         public long ProjectId { get; set; }
 
-//        [JsonProperty("remainingEstimateSeconds")]
-//        public long? RemainingEstimateSeconds { get; set; }
-
-//        [JsonProperty("self")]
-//        public string Self { get; set; }
-
         [JsonProperty("summary")]
         public string Summary { get; set; }
     }
@@ -70,12 +46,6 @@ namespace jr.common.Jira.Models
 
     public class Author
     {
-//        [JsonProperty("displayName")]
-//        public string DisplayName { get; set; }
-
-//        [JsonProperty("key")]
-//        public string Key { get; set; }
-
         [JsonProperty("name")]
         public string Name { get; set; }
     }
