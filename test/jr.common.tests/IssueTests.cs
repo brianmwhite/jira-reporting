d2d0936d670a74bf297c32587410c38639ec690e
Brian White <brian@thirdember.com>
2017-12-26 15:06:46 -0500

    add test for combinedissuename property in issue

---
+++ test/jr.common.tests/IssueTests.cs
@@ -0,0 +1,20 @@
+﻿using jr.common.Models;
+using Xunit;
+
+namespace jr.common.tests
+{
+    public class IssueTests
+    {
+        [Fact]
+        public void IssueCombinedIssueNamePropertyTest()
+        {
+            var i = new Issue
+            {
+                IssueKey = "ABC-123",
+                IssueName = "This is my issue name"
+            };
+            const string expectedCombinedIssueName = "[ABC-123] This is my issue name";
+            Assert.Equal(expectedCombinedIssueName, i.CombinedIssueName);
+        }
+    }
+}
